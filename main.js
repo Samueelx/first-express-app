c8b631e4dc9063c4f5c318a1ac506f533125c7ce
Samueelx <gsammygitau@gmail.com>
2022-01-14 23:45:50 +0300

    Added a POST method to receive user submitted data.

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {
-    res.send("Welcome to our homepage!");
-})
+    res.send(`
+    <h1>What is the color of the sky on a clear day?</h1>
+    <form action = "/result" method = "POST">
+        <input type = "text" name = "color">
+        <button>Submit answer</button>
+    </form>
+    `);
+});
+
 app.get('/about', (req, res) => {
     res.send("Thanks for learning more about us!");
-})
+});
+
+app.post("/result", (req, res) => {
+    if (req.body.color === "blue"){
+        res.send("Congrats, that is correct.");
+    }
+    else {
+        res.send("Incorrect, please try again!");
+    }
+});
 
 app.listen(3000);
