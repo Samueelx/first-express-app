38cef0a7bc9182cc7342bbc2b37484662b60f834
Samueelx <gsammygitau@gmail.com>
2022-01-15 14:20:59 +0300

    added some basic styling

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
